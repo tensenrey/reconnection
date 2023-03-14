894eb1ec00a8688636d61bd3849d35d4db17f5b9
DmitryqV <vlasenko_dima@icloud.com>
2023-03-14 14:29:47 +0500

    remove auth model

--- backend/src/authorization/authorization.module.ts
+++ backend/src/authorization/authorization.module.ts
@@ -1,19 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthorizationController } from './authorization.controller';
-import { TypegooseModule } from 'nestjs-typegoose';
-import { AuthModel } from '../models/auth.model';
 
 @Module({
   controllers: [AuthorizationController],
-  imports: [
-    TypegooseModule.forFeature([
-      {
-        typegooseClass: AuthModel,
-        schemaOptions: {
-          collection: 'Auth',
-        },
-      },
-    ]),
-  ],
 })
 export class AuthorizationModule {}

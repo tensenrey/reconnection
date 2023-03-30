6f4e0e4f67b3c594015776935c252a333d4e69ae
DmitryqV <vlasenko_dima@icloud.com>
2023-03-30 18:30:12 +0500

    [UI] Update subscribe button

--- frontend/src/components/person__card/person__card.component.tsx
+++ frontend/src/components/person__card/person__card.component.tsx
@@ -24,7 +24,7 @@ export const PersonCard: FunctionComponent = () => {
             <div>
               <b className={s.username}>{username}</b>
               <ul className={s.social}>
-                <li className={s.social__link}>Subscribe</li>
+                <li className={s.social__link}>subscribe</li>
               </ul>
             </div>
           </div>

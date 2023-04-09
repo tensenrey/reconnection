cc73a7c5c08e9b75ada14afb82a25b9e4927784b
DmitryqV <vlasenko_dima@icloud.com>
2023-04-09 19:00:55 +0500

    Update profile card

--- frontend/src/components/person__card/person__card.component.tsx
+++ frontend/src/components/person__card/person__card.component.tsx
@@ -8,46 +8,50 @@ interface IPersonCard {
   data: CoreTypes.User.IUserSource;
 }
 
-export const PersonCard: FunctionComponent<IPersonCard> = ({ data }) => (
-  <section className={s.poster}>
-    <div className={s.poster__layout}>
-      <div className={s.user__group}>
-        <div className={s.user__meta__info}>
-          <div className={s.avatar__wrapper}>
-            {data?.avatar ? (
-              <img className={s.avatar} src={data.avatar} />
-            ) : (
-              <div className={s.avatar}>{data?.username![0]}</div>
-            )}
-            {/* <div className={s.verify}>
+export const PersonCard: FunctionComponent<IPersonCard> = ({ data }) => {
+  const { id } = Utils.JWTDecoder(localStorage.getItem("secret")!);
+
+  return (
+    <section className={s.poster}>
+      <div className={s.poster__layout}>
+        <div className={s.user__group}>
+          <div className={s.user__meta__info}>
+            <div className={s.avatar__wrapper}>
+              {data?.avatar ? (
+                <img className={s.avatar} src={data.avatar} />
+              ) : (
+                <div className={s.avatar}>{data?.username![0]}</div>
+              )}
+              {/* <div className={s.verify}>
                 <Icons.Verify />
               </div> */}
+            </div>
+            <div>
+              <b className={s.username}>{data?.username}</b>
+              <ul className={s.social}>
+                {data?.id !== id && <li className={s.social__link}>subscribe</li>}
+              </ul>
+            </div>
           </div>
-          <div>
-            <b className={s.username}>{data?.username}</b>
-            <ul className={s.social}>
-              <li className={s.social__link}>subscribe</li>
-            </ul>
-          </div>
+          <ul className={s.attach}>
+            <li className={s.attach__item}>
+              {Utils.numberParser(0)}
+              <Icons.Followers />
+            </li>
+            <li className={s.attach__item}>
+              {Utils.numberParser(0)}
+              <Icons.Reactions />
+            </li>
+            <li className={s.attach__item}>
+              {Utils.numberParser(0)}
+              <Icons.Rating />
+            </li>
+          </ul>
+          <p className={s.user__description}>
+            {data?.description || "Description not specified"}
+          </p>
         </div>
-        <ul className={s.attach}>
-          <li className={s.attach__item}>
-            {Utils.numberParser(0)}
-            <Icons.Followers />
-          </li>
-          <li className={s.attach__item}>
-            {Utils.numberParser(0)}
-            <Icons.Reactions />
-          </li>
-          <li className={s.attach__item}>
-            {Utils.numberParser(0)}
-            <Icons.Rating />
-          </li>
-        </ul>
-        <p className={s.user__description}>
-          {data?.description || "Description not specified"}
-        </p>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};

f9d9a30f6ae4c5fd5f5c4c225649d19374e4bc5b
tensenrey <tensenrey@icloud.com>
2023-08-07 12:28:10 +0500

    upd

--- frontend/src/core/core.ts
+++ frontend/src/core/core.ts
@@ -1,15 +1,23 @@
 /**
  * @author tensenrey
  */
+
+import { Utils } from "@/utils/export";
+
+interface Session {
+  access: boolean;
+}
+
 export default class ApplicationCore {
-  private readonly reactLifyCycle: Function;
+  private readonly reactLifeCycle: Function;
+  private readonly token = localStorage.getItem("secret");
 
   /**
    * Application Constructor
-   * @param reactLifyCycle 
+   * @param reactLifeCycle 
    */
-  constructor(reactLifyCycle: Function) {
-    this.reactLifyCycle = reactLifyCycle;
+  constructor(reactLifeCycle: Function) {
+    this.reactLifeCycle = reactLifeCycle;
   }
 
   /**
@@ -39,10 +47,17 @@ export default class ApplicationCore {
    */
   protected async run(): Promise<void> {
     try {
-      const layers = await Promise.all([this._guardLayer(), this.session()]);
+      const layers = await Promise.allSettled([this._guardLayer(), this.session()]);
 
       if (layers) {
+        const session = layers[1];
+
         this._runReactLifeCycleLayer();
+
+        if (session.status === "fulfilled" && session.value) {
+          const { id } = Utils.JWTDecoder(this.token!);
+          window.history.pushState({}, "reconnection", "/#/" + id);
+        }
       } else {
         throw this._ExceptionHandler("LayersNotResolved", layers);
       }
@@ -67,9 +82,15 @@ export default class ApplicationCore {
   /**
    * @description request for get user session
    */
-  private session() {
-    const token = localStorage.getItem("secret");
-    return this._makeCall<{}>(window.location.origin + "/api/auth/session", { method: "POST", body: JSON.stringify({ token }) });
+  private session(): Promise<Session | undefined> {
+
+    const request = this._makeCall<Session>(window.location.origin + "/api/auth/session", {
+      headers: {
+        "Content-Type": "application/json",
+      }, method: "POST", body: JSON.stringify({ token: this.token })
+    });
+
+    return request;
   }
 
   /**
@@ -88,7 +109,7 @@ export default class ApplicationCore {
    */
   private async _runReactLifeCycleLayer(): Promise<React.FunctionComponent> {
     try {
-      return await this.reactLifyCycle();
+      return await this.reactLifeCycle();
     } catch (e) {
       throw this._ExceptionHandler("ReactLifeCycleException", e);
     }
